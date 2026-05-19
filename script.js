db89bd0dce16b7897ad27e85de8d3d5d60d93383
Ejike Odoh <ejike.developer@gmail.com>
2026-05-19 08:20:09 +0100

    Add dropdown for desktop design

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 // Access the button
 const featuresBtn = document.querySelector("#features")
 
-// Access the icon inside the button
-const image = featuresBtn.firstElementChild
-console.dir(image)
-
 // Add hover event listener to the button
 featuresBtn.addEventListener('mouseenter', ()=> {
     // when button is hovered, change src of button to desired img
-    image.src = "images/icon-arrow-up.svg"
+    featuresBtn.firstElementChild.src = "images/icon-arrow-up.svg"
 })
 
 featuresBtn.addEventListener('mouseleave', ()=> {
     // when button is hovered, change src of button to desired img
-    image.src = "images/icon-arrow-down.svg"
+    featuresBtn.firstElementChild.src = "images/icon-arrow-down.svg"
+})
+
+
+const companyBtn = document.querySelector("#company")
+const companyIcon = companyBtn.firstElementChild
+
+companyBtn.addEventListener('mouseenter', ()=> {
+     companyIcon.src = "images/icon-arrow-up.svg"
+})
+
+companyBtn.addEventListener('mouseleave', ()=> {
+     companyIcon.src = "images/icon-arrow-down.svg"
 })
